4b76b916dce5a0d07e7d65a70b840ee677561059
282764407@qq.com <Yangjunbin@@0226>
2025-07-16 21:58:35 +0800

    update

--- adapters/platform/h5/canvas-adapter.ts
+++ adapters/platform/h5/canvas-adapter.ts
@@ -209,4 +209,9 @@ export class H5CanvasContext implements CanvasContext {
   createRadialGradient(x0: number, y0: number, r0: number, x1: number, y1: number, r1: number): CanvasGradient {
     return this.ctx.createRadialGradient(x0, y0, r0, x1, y1, r1)
   }
+
+  createCircularGradient(x: number, y: number, r: number): CanvasGradient {
+    // 起点和终点都在同一个圆心，内半径为0，外半径为r
+    return this.createRadialGradient(x, y, 0, x, y, r);
+  }
 } 
